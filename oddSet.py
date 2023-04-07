b6033ad1d724c41f3fcba3b5b903ec01c271e45f
Karangiya Jash Ramde <90103407+jashkarangiya@users.noreply.github.com>
2023-04-07 10:57:35 +0530

    Update oddSet.py

--- oddSet.py
+++ oddSet.py
@@ -1,1 +1,41 @@
-Odd Set
+# Odd Set
+
+# No code for python
+
+
+#include<bits/stdc++.h>
+# using namespace std;
+# const int N=1e5+1;
+# int n,ans[N];
+# deque<int> d[N];
+# set<int> s;
+# void solve(){
+#     cin>>n;
+#     for(int i=1;i<=n;i++){
+#         int x;
+#         cin>>x;
+#         s.insert(x);
+#         d[x].push_back(i);
+#     }
+#     for(int i=1;i<=2*n;i++){
+#         auto it=s.upper_bound(i);
+#         if(it==s.begin())continue;
+#         it--;
+#         int p=d[*it].front();
+#         d[*it].pop_front();
+#         ans[p]=i;
+#         if(d[*it].empty())s.erase(it);
+#     }
+#     for(int i=1;i<=n;i++)
+#     cout<<ans[i]<<' ';
+#     cout<<'\n';
+
+# }
+
+# int main(){
+#     int t;cin>>t;
+#     while(t--){
+#         solve();
+#     }
+#     return 0;
+# }
