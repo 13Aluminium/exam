9352dac358043bcef9f70bb7e14bc130fdb07b75
Ketul Pandya <120679959+ketul-pandya@users.noreply.github.com>
2024-11-25 10:48:15 +0530

    Create input.c

---
+++ input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// This is a single-line comment
+int main()
+{
+    /* This is a
+       multi-line comment */
+    printf("Hello, World!\n"); // Another comment
+    return 0;
+}
